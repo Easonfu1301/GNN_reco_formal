2baf85a5d7ad492f8e413a3bcf8f003af337bc0a
傅逸昇 <1113873713@qq.com>
2024-09-11 07:39:04 +0800

    add class Graph

--- pipeline_v1.py
+++ pipeline_v1.py
@@ -1,4 +1,5 @@
 from trkr.Sample import HitSample
+from trkr.Graph import Graph
 from trkr.Model.GCN import GCN
 from trkr.Train import Train
 import matplotlib.pyplot as plt
@@ -6,23 +7,28 @@ import numpy as np
 
 if __name__ == "__main__":
     # Create a sample object
-    sample = HitSample(20, 5)
+    sample = HitSample(2000, 5)
     sample.generate_samples(1)  # here we just generate one sample, but we should generate more samples
-    sample.visualie_sample()
+    # sample.visualie_sample()
     # sample.save_samples("sample_store")
     # # sample.load_samples("sample_store")
     # sample.visualie_sample()
 
-    sample.sample2graph()
+
+    graph = Graph(sample.get_samples())
+
+    graph.sample2graph()
     # # sample.save_graphs("graph_store")
     # sample.load_graphs("graph_store")
-    sample.visualize_graph()
+    # graph.visualize_graph()
 
-    # graph = sample.getgraph(0)
+    graph0 = graph.getgraph(0)
+    print(graph0.num_features)
 
-    # model = GCN(graph.num_features, 8)
+    model = GCN(graph0.num_features, 8)
     #
-    # train = Train(model, sample.gen_graphs)
+    train = Train(model, graph.gen_graphs)
     # train.train(20000, True, path="pth_store")
+    train.train(20000, True)
     print(sample)
     # print(train)
--- trkr/Sample.py
+++ trkr/Sample.py
@@ -207,63 +207,11 @@ class HitSample:
 
         pass
 
-    def sample2graph(self):
-        graphs = []
-        self.Log(f"Converting {len(graphs)} samples to graphs")
-        for sample in tqdm(self.gen_samples):
-            graphs.append(hit2graph(sample))
-            # graphs.append(hg_test(sample, self.gen_mode))
-            # t = time.time()
-            # hg_test(sample, self.gen_mode)
-            # print(time.time() - t)
-        self.gen_graphs = graphs
-        self.Log(f"Converting Done! {len(graphs)} graphs generated")
-        self.ifgraph = True
 
-        pass
-
-    def getgraph(self, index=0):
-        if not self.ifgraph:
-            self.warning("No graph generated, Therefore, no graph to return")
+    def get_samples(self):
+        if not self.ifgen:
+            self.warning("No sample generated, Therefore, no sample to return")
             return -1
-        return self.gen_graphs[index]
+        return self.gen_samples
 
-    def save_graphs(self, folder_path):
-        if not self.ifgraph:
-            self.warning("No graph generated, Therefore, no graph to save")
-            return -1
-        # save the PYG DATA
-        for i, graph in enumerate(self.gen_graphs):
-            torch.save(graph, f"{folder_path}/graph_{i}.pt")
 
-        pass
-
-    def load_graphs(self, folder_path):
-        if self.ifgraph:
-            self.warning("Using load graph... \n \t Notice the original graph will be replaced")
-
-        graphs = []
-        for file in os.listdir(folder_path):
-            if file.endswith(".pt"):
-                graph = torch.load(os.path.join(folder_path, file))
-                graphs.append(graph)
-        self.gen_graphs = graphs
-
-        self.ifgraph = True
-        self.Log(f"Load {len(graphs)} graphs from '{folder_path}'")
-
-        pass
-
-    def visualize_graph(self, index=0):
-        if not self.ifgraph:
-            self.warning("No graph generated, Therefore, no graph to visualize")
-            return -1
-        graph = self.gen_graphs[index]
-        # visualize the graph with networkx
-
-        G = to_networkx(graph, to_undirected=False)
-        pos = nx.spring_layout(G)
-        nx.draw(G, pos, with_labels=True, node_color=['skyblue'], node_size=80, font_size=10, font_color='black')
-        plt.show()
-
-        pass
